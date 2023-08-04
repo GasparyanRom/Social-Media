1734aa41d50c31dd5d2229518ff4cf80311eeca5
GasparyanRom <harutunyansuro62@gmail.com>
2023-08-04 12:09:08 +0400

    wip:cod in eror

---
+++ lib/comment_screen.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+
+class commentscreen extends StatelessWidget {
+  const commentscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              child: Row(
+                children: [
+                  Text(
+                    'Comment',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Container(
+                width: 400,
+                height: 400,
+                decoration: BoxDecoration(
+                    color: Color.fromRGBO(196, 196, 196, 1),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8),
+                          child: Image.asset('images/user.png'),
+                        ),
+                        const Text(
+                          'El kamcy speaks',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w600),
+                        )
+                      ],
+                    ),
+                    Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra ',
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.w400),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 50),
+                      child: Image.asset('images/post.png'),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [Icon(Icons.heart_broken), Icon(Icons.chat)],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.amber,
+              height: 100,
+              width: 400,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Image.asset('images/2.png'),
+                      Text(
+                        'Kamcy el',
+                        style: TextStyle(
+                            fontSize: 13, fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra aliquam, congue habitasse tortor. Fringilla nunc aliquam volutpat suscipit porttitor in quis sagittis hac. Tellus sed ac libero',
+                    style: TextStyle(fontSize: 8, fontWeight: FontWeight.w700),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Icon(Icons.heart_broken),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                color: Colors.amber,
+                height: 100,
+                width: 400,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Image.asset('images/2.png'),
+                        Text(
+                          'Kamcy el',
+                          style: TextStyle(
+                              fontSize: 13, fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra aliquam, congue habitasse tortor. Fringilla nunc aliquam volutpat suscipit porttitor in quis sagittis hac. Tellus sed ac libero',
+                      style:
+                          TextStyle(fontSize: 8, fontWeight: FontWeight.w700),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(Icons.heart_broken),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.amber,
+              height: 100,
+              width: 400,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Image.asset('images/2.png'),
+                      Text(
+                        'Kamcy el',
+                        style: TextStyle(
+                            fontSize: 13, fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pharetra aliquam, congue habitasse tortor. Fringilla nunc aliquam volutpat suscipit porttitor in quis sagittis hac. Tellus sed ac libero',
+                    style: TextStyle(fontSize: 8, fontWeight: FontWeight.w700),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Icon(Icons.heart_broken),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:socialmedia/comment_screen.dart';
 import 'package:socialmedia/live_screen.dart';
 import 'package:socialmedia/posts/addpost.dart';
 import 'package:socialmedia/chats/chat.dart';
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: livescreen(),
+      home: storyscreen(),
     );
   }
 }
